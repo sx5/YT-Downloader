1172dc18942f51199ede5af438e19cc306c6804d
sx5 <75947756+sx5@users.noreply.github.com>
2021-09-15 10:25:17 +0400

    Add files via upload

---
+++ YTdownload.py
@@ -0,0 +1,25 @@
+from pytube import YouTube
+import os
+
+def exoticisgay():
+    exotic_esex.download()
+os.system('cls & title YT DOWNLOADER')
+link = input("> Video URL: ")
+exotic_esex = YouTube(link)
+cumslut = exotic_esex.streams.first()
+
+detail = exotic_esex.streams.filter(file_extension='mp4').get_by_itag(22)
+print('"',detail.title,'" ?')
+def option():
+    choice = input("\nContinue ?(y/n)")
+    if choice == "y":
+        exoticisgay()
+    elif choice == "n":
+        input("Sorry, press any key to start a new search...")
+        os.system('cls')
+        option()
+    else:
+        input("[!] INVALID OPTION (Press any key to start a new search)")
+        option()
+option()
+#made by exotic and i cbb to continue fixing it
